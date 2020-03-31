1ee2fd62b3d07e653079c5b5700b2173ad053f53
Bolotnikov Aleksandr <aleksandr.bolotnikov@zimad.com>
2020-03-31 09:52:54 +0300

    Show how to store persistent data for a high score

--- assets/scripts/Score.js
+++ assets/scripts/Score.js
@@ -7,13 +7,25 @@ cc.Class({
         scoreLabel: {
             default: null,
             type: cc.Label
+        },
+        bestScoreLabel: {
+            default: null,
+            type: cc.Label
         }
     },
 
     // onLoad () {},
 
     start () {
+        let bestScore = localStorage.getItem('score') || 0;
+
+        if (!bestScore || Globals.score > bestScore) {
+            bestScore = Globals.score;
+            localStorage.setItem('score', bestScore);
+        }
+
         this.scoreLabel.string = `Score: ${Globals.score.toString()}`;
+        this.bestScoreLabel.string = `Best Score: ${bestScore.toString()}`;
         this.node.on(cc.Node.EventType.TOUCH_END, () => {
             cc.director.loadScene('Game');
         });
